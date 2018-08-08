818caed30294c75edeafb9d0117e09381b85709f
nic <nwerneck@gmail.com>
2018-08-09 01:07:03 +0200

    nicely working with blue skies. refactores into many small functions, top-down thanks to the declare function

--- gen-pic.clj
+++ gen-pic.clj
@@ -1,4 +1,3 @@
-
 (require '[clojure.string :as str])
 (require '[clojure.java.io :as jio])
 
@@ -9,39 +8,71 @@
 (def oc [(+ 0.5 (/ IW -2)) (+ 0.5 (/ IH -2)) (/ IW 2)])
 (def tilt 0.6)
 
-(defn touch-sphere? [cw vv]
-  (let [
-        oo [0 -1.5 -0.6]
-        rr 0.6
-        ll (normalze vv)
-        oc (vsub oo cw)
-        aa 1
-        bb (* 2 (dot ll oc))
-        cc (- (dot oc oc) (* rr rr))
-        delt (- (* bb bb) (* 4 aa cc))]
-    (def dd (/ (- bb (Math/sqrt delt)) 2))
-    (and (> delt 0)
-         (> 0 (+ (get cw 2) (* (get ll 2) dd)))
-         )))
-    
+(def sky-color [63 126 226])
+(def plane-white [255 255 255])
+(def plane-black [0 0 0])
+
+(def oo [0 -1.5 -0.6])
+(def rr 0.6)
+
+(declare camera-model find-color touch-sphere? trace-reflection plane-texture trace-if-hit sky-or-plane)
+
+(def pix
+  (for [j (range IH)
+        k (range IW)]
+    (camera-model j k)))
 
 (defn camera-model [j k]
   (let [cw [0 0 -2.0]
         qq [(- 1 (* tilt tilt)) (* tilt tilt) 0 0]
-        vv (rotate qq (vsum [k j 0] oc))
-        t (/ (* -1 (get cw 2)) (get vv 2))]
-    
-    (if (< t 0)
-      128
-      (let [px (+ (get cw 0) (* t(get vv 0)))
-            py (+ (get cw 1) (* t(get vv 1)))]
-        (if (touch-sphere? cw vv)
-          200
-          (if (or (and (< (mod px 1.0) 0.5) (< (mod py 1.0) 0.5)) 
-                  (and (> (mod px 1.0) 0.5) (> (mod py 1.0) 0.5)))
-            255
-            0))))))
+        vv (rotate qq (vsum [k j 0] oc))]
+    (find-color cw vv 1)))
 
+(defn find-color [cw vv iter]
+  (or (trace-if-hit cw vv iter)
+      (sky-or-plane cw vv)))
+
+(defn trace-if-hit [cw vv iter]
+  (let [dd (touch-sphere? cw vv)]
+    (if dd
+      (trace-reflection cw dd vv iter)
+      nil)))
+
+(defn trace-reflection [cw dd vv iter]
+  (let [newcw (vsum cw (vscale dd (normalze vv)))
+        nn (vsub newcw oo)]
+    (if (< 0 iter)
+        (find-color newcw (reflect vv nn) (- iter 1))
+        0)))
+
+(defn sky-or-plane [cw vv]
+  (let [t (/ (* -1 (get cw 2)) (get vv 2))]
+    (if (< t 0)
+      sky-color
+      (plane-texture cw vv t))))
+    
+(defn plane-texture [cw vv t]  
+  (let [px (+ (get cw 0) (* t (get vv 0)))
+        py (+ (get cw 1) (* t (get vv 1)))]          
+     (if (> (norm [px py 0]) 50)
+       sky-color
+       (if (or (and (< (mod px 1.0) 0.5) (< (mod py 1.0) 0.5)) 
+               (and (> (mod px 1.0) 0.5) (> (mod py 1.0) 0.5)))
+           (map int (vscale (/ 0.001 (* t t)) plane-white))      
+           (map int (vscale (/ 0.001 (* t t)) plane-black))))))
+
+(defn touch-sphere? [cw vv]
+  (let [ll (normalze vv)
+        oc (vsub cw oo)
+        bb (dot ll oc)
+        cc (- (dot oc oc) (* rr rr))
+        delt (- (* bb bb) cc)    
+        dd (- (* -1 bb) (Math/sqrt delt))]
+    (if (and (> delt 0)
+             (> 0 (+ (get cw 2) (* (get ll 2) dd)))
+             (< 0 dd))
+      dd
+      nil)))
 
 ;; (def pix
 ;;   (for [j (range IH)
@@ -50,13 +81,7 @@
 ;;           (+ 1e-7 (* 0.2 (Math/sqrt (+ (* k k) (* j j)))))]
 ;;       (int (* 255  (+ 0.5 ( * 0.5 (/ (Math/sin ( * (* 0.5 Math/PI) r))
 ;;                                      (Math/sqrt r)))))))))
-
-(def pix
-  (for [j (range IH)
-        k (range IW)]
-    (camera-model j k)
-    ))
-
+    
 ;; output netbpm file
 (defn write-to-file [file-name contents]
   (with-open [wtr (jio/writer file-name)]
@@ -66,11 +91,11 @@
 (defn joined-values [vv]
   (str/join " "  vv))
 
-(def p6-header ["P2" (str IW " " IH) "255"])
+(def p6-header ["P3" (str IW " " IH) "255"])
 
 (def final-file-rows
   (conj p6-header
-        (joined-values pix)
+        (joined-values (flatten pix))
         ""))
 
 (def final-file-contents
--- quat.clj
+++ quat.clj
@@ -1,3 +1,4 @@
+(declare norm normalze dot vscale vsum vsub)
 
 (defn qrot [a b c d]
 
@@ -9,15 +10,31 @@
         bc (* b c)        bd (* b d)        cc (* c c)        cd (* c d)        dd (* d d)]    
     [[(- (+ aa bb) cc dd),      (* 2 (- bc ad)),      (* 2 (+ bd ac))],     
      [(* 2 (+ bc ad)),      (- (+ aa cc) bb dd),      (* 2 (- cd ab))],
-     [(* 2 (- bd ac)),      (* 2 (+ cd ab)),      (- (+ aa dd) bb cc)]]
-    )
-  )
+     [(* 2 (- bd ac)),      (* 2 (+ cd ab)),      (- (+ aa dd) bb cc)]]))
+    
+(defn rotate [qq vec]
+  (let [qr (apply qrot qq)
+        qx (get qr 0)
+        qy (get qr 1)
+        qz (get qr 2)]
+    [(dot qx vec)
+     (dot qy vec)
+     (dot qz vec)]))
+  
+(defn reflect[dd nn]
+  (vsub dd (vscale (* 2 (dot dd nn))
+                   (normalze nn))))
+
+(defn normalze [vv]
+  (vec (map #(/ % (norm vv)) vv)))       
+
+(defn norm [va]
+  (Math/sqrt (dot va va)))
 
 (defn dot [va vb]
   (+ (* (get va 0) (get vb 0))
      (* (get va 1) (get vb 1))
-     (* (get va 2) (get vb 2))
-     ))
+     (* (get va 2) (get vb 2))))
 
 (defn vsum [va vb]
   [(+ (get va 0) (get vb 0))
@@ -29,29 +46,13 @@
    (- (get va 1) (get vb 1))
    (- (get va 2) (get vb 2))])
 
-(defn norm [va]
-  (Math/sqrt (dot va va)))
-
-(defn normalze [vv]
-  (vec (map #(/ % (norm vv)) vv)
-       ))
-
-(defn rotate [qq vec]
-  (let [qr (apply qrot qq)
-        qx (get qr 0)
-        qy (get qr 1)
-        qz (get qr 2)]
-    [(dot qx vec)
-     (dot qy vec)
-     (dot qz vec)]
-    ))
-
-
+(defn vscale [s vv]
+  (vec (map #(* s %) vv)))
+    
 (def myq [(- 1 (* 0.1 0.1))          
           (* 0.1 0.1)
-          0 0]
-  )
-
+          0 0])
+  
 (def myv [0.0 1.0 0.0])
 
 ;; (println myq)
